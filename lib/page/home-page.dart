c3ec05f519e0c8aeaac367135e30189b98557948
Carlos Garay <carlosgaraymoreno1@gmail.com>
2021-04-23 21:29:01 -0500

    setState botones

--- lib/page/home-page.dart
+++ lib/page/home-page.dart
@@ -6,6 +6,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  String operaciones = "";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,49 +20,123 @@ class _HomePageState extends State<HomePage> {
   Widget _body() {
     return Column(
       children: [
+        Expanded(
+          child: Container (
+            color:Colors.red,
+            ),
+        ),
         Container(
-          color: Colors.red,
+          color: Colors.blue,          
         ),
         Container(
           color: Colors.blue,
           height: 100,
+          child: Row(
+            children: [
+              Text(operaciones),
+            ],
+          ),
         ),
         Container(
           child: Column(children: [
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
-                ElevatedButton(onPressed: () {}, child: Text('7')),
-                ElevatedButton(onPressed: () {}, child: Text('8')),
-                ElevatedButton(onPressed: () {}, child: Text('9')),
-                ElevatedButton(onPressed: () {}, child: Text('/')),
+                ElevatedButton(onPressed: () {
+                  setState(() {
+                    operaciones += "7";
+                  });
+                }, child: Text('7')),
+                ElevatedButton(onPressed: () {
+                  setState(() {
+                    operaciones += "8";
+                  });
+                }, child: Text('8')),
+                ElevatedButton(onPressed: () {
+                  setState(() {
+                    operaciones += "9";
+                  });
+                }, child: Text('9')),
+                ElevatedButton(onPressed: () {
+                  setState(() {
+                    operaciones += "7";
+                  });
+                }, child: Text('/')),
               ],
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
-                ElevatedButton(onPressed: () {}, child: Text('4')),
-                ElevatedButton(onPressed: () {}, child: Text('5')),
-                ElevatedButton(onPressed: () {}, child: Text('6')),
-                ElevatedButton(onPressed: () {}, child: Text('*')),
+                ElevatedButton(onPressed: () {
+                  setState(() {
+                    operaciones += "4";
+                  });
+                }, child: Text('4')),
+                ElevatedButton(onPressed: () {
+                  setState(() {
+                    operaciones += "5";
+                  });
+                }, child: Text('5')),
+                ElevatedButton(onPressed: () {
+                  setState(() {
+                    operaciones += "6";
+                  });
+                }, child: Text('6')),
+                ElevatedButton(onPressed: () {
+                  setState(() {
+                    operaciones += "*";
+                  });
+                }, child: Text('*')),
               ],
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
-                ElevatedButton(onPressed: () {}, child: Text('1')),
-                ElevatedButton(onPressed: () {}, child: Text('2')),
-                ElevatedButton(onPressed: () {}, child: Text('3')),
-                ElevatedButton(onPressed: () {}, child: Text('-')),
+                ElevatedButton(onPressed: () {
+                  setState(() {
+                    operaciones += "1";
+                  });
+                }, child: Text('1')),
+                ElevatedButton(onPressed: () {
+                  setState(() {
+                    operaciones += "2";
+                  });
+                }, child: Text('2')),
+                ElevatedButton(onPressed: () {
+                  setState(() {
+                    operaciones += "3";
+                  });
+                }, child: Text('3')),
+                ElevatedButton(onPressed: () {
+                  setState(() {
+                    operaciones += "-";
+                  });
+                }, child: Text('-')),
               ],
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
-                ElevatedButton(onPressed: () {}, child: Text('0')),
-                ElevatedButton(onPressed: () {}, child: Text('C')),
-                ElevatedButton(onPressed: () {}, child: Text('=')),
-                ElevatedButton(onPressed: () {}, child: Text('+')),
+                ElevatedButton(onPressed: () {
+                  setState(() {
+                    operaciones += "0";
+                  });
+                }, child: Text('0')),
+                ElevatedButton(onPressed: () {
+                  setState(() {
+                    operaciones += "C";
+                  });
+                }, child: Text('C')),
+                ElevatedButton(onPressed: () {
+                  setState(() {
+                    operaciones += "=";
+                  });
+                }, child: Text('=')),
+                ElevatedButton(onPressed: () {
+                  setState(() {
+                    operaciones += "+";
+                  });
+                }, child: Text('+')),
               ],
             )
           ]),
